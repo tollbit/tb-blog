24c77fa20a3d315dfe21d57877b8b83980a0f87f
Josh Mayer <jooshmayer@gmail.com>
2023-11-29 21:17:08 -0500

    toolbooth testing

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,10 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const token = request.headers.get("tollbit-token");
-
-  if (!token) {
-    return new Response("403 Forbidden Access", {
-      status: 403,
-    });
-  }
+  //   const token = request.headers.get("tollbit-token");
+  //   if (!token) {
+  //     return new Response("403 Forbidden Access", {
+  //       status: 403,
+  //     });
+  //   }
 }
