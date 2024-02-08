a62604cd2ca81997d3e7ac2f1d808d47946bc566
Josh Mayer <jooshmayer@gmail.com>
2024-02-08 15:04:16 -0500

    for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,14 +9,14 @@ const imperial = Imperial_Script({ weight: ["400"], subsets: ["latin"] });
 export default function Header() {
   return (
     <header className="">
-      <nav className="fixed top-0 flex justify-between w-full items-center  dark:mix-blend-difference px-4 sm:px-10 pt-8 font-bold">
-        <a href="mailto:josh@tollb.it" className="hover:underline">
-          Contact
+      <nav className="fixed top-0 flex justify-between w-full items-center dark:mix-blend-difference px-4 sm:px-10 pt-8 font-bold">
+        <a href="mailto:josh@tollb.it" className="hover:underline hidden">
+          Contacts
         </a>
         <div className="text-2xl space-x-6">
           <Link
             href="/"
-            className="hover:bg-gray-100 hover:dark:bg-gray-900 px-4 py-4 rounded-lg transition ease-in-out"
+            className="sm:hover:bg-gray-100 sm:hover:dark:bg-gray-900 sm:px-4 py-4 rounded-lg transition ease-in-out"
           >
             {" "}
             <span className={`${imperial.className} text-5xl`}>why</span>
