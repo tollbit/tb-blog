7ec7ac3d7fac13ed2bfcda9e78ae8e4d6cf7ef56
Josh Mayer <jooshmayer@gmail.com>
2024-02-12 12:44:55 -0500

    new logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Inter, Pinyon_Script, Imperial_Script } from "next/font/google";
+import Image from "next/image";
 
 const pinyon = Pinyon_Script({ weight: ["400"], subsets: ["latin"] });
 
@@ -9,7 +10,7 @@ const imperial = Imperial_Script({ weight: ["400"], subsets: ["latin"] });
 export default function Header() {
   return (
     <header className="">
-      <nav className="fixed top-0 flex justify-between w-full items-center dark:mix-blend-difference px-4 sm:px-10 pt-8 font-bold">
+      <nav className="fixed top-0 flex justify-between w-full items-center dark:mix-blend-difference px-4 sm:px-10 font-bold">
         <a
           href="mailto:josh@tollb.it"
           className="hover:underline hidden sm:block"
@@ -17,13 +18,14 @@ export default function Header() {
           Contact
         </a>
         <div className="text-2xl space-x-6">
-          <Link
-            href="/"
-            className="sm:hover:bg-gray-100 sm:hover:dark:bg-gray-900 sm:px-4 py-4 rounded-lg transition ease-in-out"
-          >
-            {" "}
-            <span className={`${imperial.className} text-5xl`}>why</span>
-            .tollbit.com
+          <Link href="/" className="sm:px-4 rounded-lg transition ease-in-out">
+            <Image
+              src={"/whylogo2.svg"}
+              alt={"Why Tollbit Logo"}
+              width={220}
+              height={220}
+              className="text-foreground"
+            />
           </Link>
         </div>
         {/* <Link href="/posts">Notes</Link> */}
