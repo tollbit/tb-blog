c045ae3b21e08c056bfbd47750be6e24a2cb6e7a
Josh Mayer <jooshmayer@gmail.com>
2024-02-22 12:33:09 -0500

    backdrop blur

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,22 +2,18 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="fixed bottom-0 w-full px-4 sm:px-10 pb-8 dark:mix-blend-difference font-bold tracking-tighter">
-        <nav className="text-sm flex justify-between space-x-6">
-          <div className="">
-            <h3 className="">Novoscribe, Inc. © 2024.</h3>
-          </div>
-          <div className="space-x-6 underline">
-            <Link href="https://tollbit.com">tollbit.com</Link>
-            {/* <Link href="https://x.com/tollbit_">X</Link> */}
-            {/* <Link href="https://github.com/">GitHub</Link> */}
-            <Link href="https://www.linkedin.com/company/tollbit/">
-              LinkedIn
-            </Link>
-          </div>
-        </nav>
-      </div>
+    <footer className="fixed bottom-0 w-full px-4 z-30 sm:px-10 py-4 sm:py-8 tracking-tighter backdrop-blur-sm bg-white/40 sm:backdrop-blur-none sm:bg-transparent">
+      <nav className="text-sm flex justify-between space-x-6 font-bold">
+        <div className="">
+          <h3 className="">Novoscribe, Inc. © 2024.</h3>
+        </div>
+        <div className="space-x-6 underline">
+          <Link href="https://tollbit.com">tollbit.com</Link>
+          {/* <Link href="https://x.com/tollbit_">X</Link> */}
+          {/* <Link href="https://github.com/">GitHub</Link> */}
+          <Link href="https://www.linkedin.com/company/tollbit/">LinkedIn</Link>
+        </div>
+      </nav>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="">
-      <nav className="fixed top-0 py-8 flex justify-between w-full items-center dark:mix-blend-difference px-4 sm:px-10 font-bold z-30">
+      <nav className="fixed top-0 py-8 flex justify-between w-full items-center px-4 sm:px-10 font-bold z-30">
         <Link href="/" className="underline hidden sm:block">
-          Home
+          Homes
         </Link>
         <Link href={"/"}>
-          <Image src={"/whytollbit.png"} alt={""} width={200} height={200} />
+          <Image src={"/whytollbit.png"} alt={""} width={200} height={50} />
         </Link>
-        <a href="mailto:josh@tollb.it" className="underline">
+        <a href="mailto:team@tollbit.com" className="underline sm:mt-0 mt-3">
           Contact
         </a>
       </nav>
